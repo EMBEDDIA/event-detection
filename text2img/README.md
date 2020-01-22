dabf463b32140fc503909b774b1d8356d2e03372
Emanuela Boros <emanuela.boros@gmail.com>
2020-01-22 16:29:19 +0100

    text2img (Khoa) - modified

---
+++ text2img/README.md
@@ -0,0 +1,14 @@
+Project forked from https://github.com/nnkhoa/text2ImgDoc. Modified to support Unicode.
+
+# Text To Image Documents script
+
+Requirement: ImageMagick 
+
+Instruction: 
+> python txt2ImgDoc.py \<input folder\>
+
+\<input folder\> is the directory containing all of the text files as well as sub-directories (two or more levels of sub-folder may cause crashes. It's best to not have any sub-directory at all).
+
+This will create two new folders: One containing the clean text file without any Html tags, the other having all of the text files converted into images.
+
+Note: Removing Html tags currently only works with articles from DAniEL dataset. Just comment out line #82 if you already have a clean dataset.
---
+++ text2img/text2img.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+
+"""
+@Author: https://github.com/nnkhoa/text2ImgDoc
+Modified by @EmanuelaBoros (support chinese and greek)
+"""
+
+import os
+import re
+import sys
+import codecs
+import subprocess
+
+
+def output_content(content, output_file, writing_type='w+'):
+    with open(output_file, writing_type) as f:
+        f.write(content)
+
+
+def create_dir(dir_name):
+    try:
+        os.mkdir(dir_name)
+        print("Directory " + dir_name + " created")
+    except FileExistsError:
+        print("Directory " + dir_name + " existed")
+
+
+def remove_tag(input_file, output_file):
+    with codecs.open(input_file, 'r', 'utf-8') as f:
+        text = f.read().strip()
+    notag_text = re.sub('<.*?>', '', text)
+    output_content(notag_text, output_file, 'w+')
+
+
+def txt_to_img(input_file, output_file):
+    print(input_file)
+    with codecs.open(input_file, 'r', 'utf-8') as f:
+        text = f.read().strip()
+
+    command = 'convert -size 1240x -fill black -pointsize 24 -fill black -verbose -font Arial-Unicode-Regular.ttf '.split(
+        ' ')
+
+    text = text.replace("'", ' ')
+    text = text.replace('"', ' ')
+    command.append('caption:"' + text + '"')
+    command.append(output_file)
+
+    stream = os.popen(' '.join(command))
+    output = stream.read()
+#    print(output)
+#    output
+
+#    import pdb;pdb.set_trace()
+#    p = subprocess.Popen(command, stdout=subprocess.PIPE)
+#    output, error = p.communicate()
+#    print(output)
+#
+#    if p.returncode != 0:
+#        output_content(error, "log", 'a+')
+
+
+def walk_through_dir(in_dir, out_dir, convert_func):
+    for dir_path, dir_names, file_names in os.walk(in_dir):
+        if len(dir_names) > 0:
+            for dir_name in dir_names:
+                sub_out_dir = os.path.join(out_dir, dir_path, dir_name)
+                create_dir(sub_out_dir)
+        else:
+            sub_out_dir = os.path.join(out_dir, os.path.basename(dir_path))
+            convert_files(dir_path, sub_out_dir, convert_func)
+
+
+def convert_files(input_dir, output_dir, convert_func):
+    for doc_file in os.listdir(input_dir):
+        if doc_file.startswith('.'):
+            continue
+
+        infile = os.path.join(os.path.abspath(input_dir), doc_file)
+
+        file_extension = ''
+        if convert_func.__name__ == "txt_to_img":
+            file_extension = ".png"
+
+        outfile = os.path.join(os.path.abspath(
+            output_dir), doc_file + file_extension)
+
+        convert_func(infile, outfile)
+
+
+if __name__ == "__main__":
+    doc_dir = sys.argv[1]
+    out_dir = "output"
+    notag_dir = out_dir + "/notag"
+    png_dir = out_dir + "/png"
+
+    create_dir(out_dir)
+    create_dir(notag_dir)
+    create_dir(png_dir)
+
+    create_dir(notag_dir + "/" + os.path.basename(doc_dir))
+    create_dir(png_dir + "/" + os.path.basename(doc_dir))
+
+    if os.path.exists(doc_dir):
+        walk_through_dir(doc_dir, notag_dir, remove_tag)
+        walk_through_dir(os.path.join(
+            notag_dir, os.path.basename(doc_dir)), png_dir, txt_to_img)
